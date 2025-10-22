92d7e958d74dbc37ee2135db6aff946c4ceb5963
qkrtkdgus010208 <psh010208@naver.com>
2025-10-22 16:49:29 +0900

    feat: 1-14. 연출과 꾸미기 적용

--- Assets/Scripts/TheStack.cs
+++ Assets/Scripts/TheStack.cs
@@ -42,6 +42,8 @@ public class TheStack : MonoBehaviour
     private const string BestScoreKey = "BestScore";
     private const string BestComboKey = "BestCombo";
 
+    private bool isGameOver = false;
+
     void Start()
     {
         if (originBlock == null)
@@ -62,6 +64,9 @@ public class TheStack : MonoBehaviour
 
     void Update()
     {
+        if (isGameOver)
+            return;
+
         if (Input.GetMouseButtonDown(0))
         {
             if (PlaceBlock())
@@ -73,6 +78,8 @@ public class TheStack : MonoBehaviour
                 // 게임 오버
                 Debug.Log("GameOver");
                 UpdateScore();
+                isGameOver = true;
+                GameOverEffect();
             }
         }
 
@@ -293,6 +300,31 @@ public class TheStack : MonoBehaviour
 
             PlayerPrefs.SetInt(BestScoreKey, bestScore);
             PlayerPrefs.SetInt(BestComboKey, bestCombo);
+        }
+    }
+
+    void GameOverEffect()
+    {
+        int childCount = this.transform.childCount;
+
+        for (int i = 1; i < 20; i++)
+        {
+            if (childCount < i)
+                break;
+
+            GameObject go =
+                this.transform.GetChild(childCount - i).gameObject;
+
+            if (go.name.Equals("Rubble"))
+                continue;
+
+            Rigidbody rigid = go.AddComponent<Rigidbody>();
+
+            rigid.AddForce(
+                (Vector3.up * Random.Range(0, 10f)
+                 + Vector3.right * (Random.Range(0, 10f) - 5f))
+                * 100f
+            );
         }
     }
 }
